9cebe884ee1473d123822b645f2183bbc8c32f67
Yakiyo <yakiyo1206@gmail.com>
2024-10-26 19:58:17 +0600

    refactor: move to diff file

--- anilist/lib/src/types.dart
+++ anilist/lib/src/types.dart
@@ -1,12 +1,3 @@
-import 'package:json_annotation/json_annotation.dart';
 
 export 'types/media.dart';
-
-/// Type of media
-@JsonEnum()
-enum MediaType {
-  @JsonValue("ANIME")
-  anime,
-  @JsonValue("MANGA")
-  manga
-}
+export 'types/enums.dart';
---
+++ anilist/lib/src/types/enums.dart
@@ -0,0 +1,25 @@
+import 'package:json_annotation/json_annotation.dart';
+
+/// Type of media
+@JsonEnum(fieldRename: FieldRename.screamingSnake)
+enum MediaType {
+  anime,
+  manga
+}
+
+/// Format of media
+@JsonEnum(fieldRename: FieldRename.screamingSnake)
+enum MediaFormat {
+  tv,
+  @JsonValue("TV_SHORT")
+  tvShort,
+  movie,
+  special,
+  ova,
+  ona,
+  music,
+  manga,
+  novel,
+  @JsonValue("ONE_SHOT")
+  oneShot
+}
