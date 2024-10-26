4bd0387b8b68c2a9666ba41c24e7d87cb81c063e
Yakiyo <yakiyo1206@gmail.com>
2024-10-26 20:44:26 +0600

    chore: impromptu commit

--- anilist/lib/src/types/media.g.dart
+++ anilist/lib/src/types/media.g.dart
@@ -18,8 +18,24 @@ Media _$MediaFromJson(Map<String, dynamic> json) => Media()
   ..episodes = (json['episodes'] as num?)?.toInt()
   ..chapters = (json['chapters'] as num?)?.toInt()
   ..volumes = (json['volumes'] as num?)?.toInt()
+  ..countryOfOrigin = json['countryOfOrigin'] as String
+  ..isLicensed = json['isLicensed'] as bool
+  ..source = json['source'] as String
+  ..hashtag = json['hashtag'] as String
+  ..trailer = Map<String, String>.from(json['trailer'] as Map)
+  ..updatedAt = (json['updatedAt'] as num).toInt()
   ..coverImage = Map<String, String>.from(json['coverImage'] as Map)
-  ..bannerImage = json['bannerImage'] as String;
+  ..bannerImage = json['bannerImage'] as String
+  ..genres = (json['genres'] as List<dynamic>).map((e) => e as String).toList()
+  ..synonyms =
+      (json['synonyms'] as List<dynamic>).map((e) => e as String).toList()
+  ..averageScore = (json['averageScore'] as num).toInt()
+  ..meanScore = (json['meanScore'] as num).toInt()
+  ..popularity = (json['popularity'] as num).toInt()
+  ..isLocked = json['isLocked'] as bool
+  ..trending = (json['trending'] as num).toInt()
+  ..favourites = (json['favourites'] as num).toInt()
+  ..tags = json['tags'] as List<dynamic>;
 
 Map<String, dynamic> _$MediaToJson(Media instance) => <String, dynamic>{
       'id': instance.id,
@@ -33,8 +49,23 @@ Map<String, dynamic> _$MediaToJson(Media instance) => <String, dynamic>{
       'episodes': instance.episodes,
       'chapters': instance.chapters,
       'volumes': instance.volumes,
+      'countryOfOrigin': instance.countryOfOrigin,
+      'isLicensed': instance.isLicensed,
+      'source': instance.source,
+      'hashtag': instance.hashtag,
+      'trailer': instance.trailer,
+      'updatedAt': instance.updatedAt,
       'coverImage': instance.coverImage,
       'bannerImage': instance.bannerImage,
+      'genres': instance.genres,
+      'synonyms': instance.synonyms,
+      'averageScore': instance.averageScore,
+      'meanScore': instance.meanScore,
+      'popularity': instance.popularity,
+      'isLocked': instance.isLocked,
+      'trending': instance.trending,
+      'favourites': instance.favourites,
+      'tags': instance.tags,
     };
 
 const _$MediaTypeEnumMap = {
