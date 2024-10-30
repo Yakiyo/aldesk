7554bcfc58b809965813ce78c41ff7f022f56756
Yakiyo <yakiyo1206@gmail.com>
2024-10-30 16:02:54 +0600

    feat: first successful request api

--- anilist/lib/src/anilist.dart
+++ anilist/lib/src/anilist.dart
@@ -1,15 +1,32 @@
 import 'package:anilist/src/client.dart';
+import 'package:anilist/src/graphql/__generated__/media.data.gql.dart';
+import 'package:anilist/src/graphql/__generated__/media.req.gql.dart';
+import 'package:anilist/src/request.dart';
 import 'package:ferry/ferry.dart';
+import 'package:option_result/option_result.dart';
 
 /// The main class to interact with the Anilist API
 class AnilistClient {
   /// User authentication token
-  final String token;
+  final String? token;
 
   // ignore: unused_field
   late final Client _client;
 
-  AnilistClient({required this.token}) {
+  AnilistClient({this.token}) {
     _client = createClient(token: token);
+  }
+
+  Future<Result<GMediaMinimalData_Media?, ApiError>> mediaMinimal(
+      {int? id, String? search}) async {
+    final query = GMediaMinimalReq(
+      (b) => b
+        ..vars.id = id
+        ..vars.search = search,
+    );
+    final res = await request(query, _client);
+    return res.map(
+      (d) => d?.Media,
+    );
   }
 }
--- anilist/test/anilist_test.dart
+++ anilist/test/anilist_test.dart
@@ -1,27 +1,17 @@
-import 'dart:convert';
-
-import 'package:anilist/src/client.dart';
-import 'package:anilist/src/graphql/__generated__/user.req.gql.dart';
-import 'package:gql_http_link/gql_http_link.dart';
+import 'package:anilist/anilist.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test("Ferry query test", () async {
-    final client = createClient();
-    final req = GGetUserReq(
-        (b) => b..vars.search = "Yakiyo",
-          );
+  final client = AnilistClient();
 
-    final res = await client.request(req).first;
-    print((res.linkException as HttpLinkServerException)
-        .parsedResponse
-        ?.errors
-        ?.map((e) => JsonEncoder.withIndent(' ').convert({
-              "message": e.message,
-              "location": e.locations
-                  ?.map((x) => "line: ${x.line}, column: ${x.column}")
-                  .join(""),
-            })));
-    print(res.linkException?.runtimeType);
+  test("Ferry query test", () async {
+    final res = await client.mediaMinimal(search: "Seirei Gensouki");
+
+    if (res.isErr()) {
+      print(res.unwrapErr());
+      return;
+    }
+
+    print(res.unwrap());
   });
 }
