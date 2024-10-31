04505372a09d8aea8b6ed70d2b030c8e1889cc0c
Yakiyo <yakiyo1206@gmail.com>
2024-10-31 20:23:34 +0600

    feat: viewer function

--- anilist/lib/src/anilist.dart
+++ anilist/lib/src/anilist.dart
@@ -1,6 +1,8 @@
 import 'package:anilist/src/client.dart';
 import 'package:anilist/src/graphql/__generated__/media.data.gql.dart';
 import 'package:anilist/src/graphql/__generated__/media.req.gql.dart';
+import 'package:anilist/src/graphql/__generated__/user.data.gql.dart';
+import 'package:anilist/src/graphql/__generated__/user.req.gql.dart';
 import 'package:anilist/src/request.dart';
 import 'package:ferry/ferry.dart';
 import 'package:option_result/option_result.dart';
@@ -17,7 +19,21 @@ class AnilistClient {
     _client = createClient(token: token);
   }
 
-  Future<Result<GMediaMinimalData_Media?, ApiError>> mediaMinimal(
+  Future<Result<GViewerData_Viewer, ApiError>> currentUser() async {
+    if (token == null || token!.isEmpty) {
+      return Err([
+        {
+          "message": "missing token, no user currently authorized",
+          "location": "unknown",
+        }
+      ]);
+    }
+
+    final res = await request(GViewerReq(), _client);
+    return res.map((d) => d!.Viewer!);
+  }
+
+  Future<Result<GMediaMinimalData_Media, ApiError>> mediaMinimal(
       {int? id, String? search}) async {
     final query = GMediaMinimalReq(
       (b) => b
@@ -26,7 +42,7 @@ class AnilistClient {
     );
     final res = await request(query, _client);
     return res.map(
-      (d) => d?.Media,
+      (d) => d!.Media!,
     );
   }
 }
--- anilist/test/anilist_test.dart
+++ anilist/test/anilist_test.dart
@@ -1,17 +1,35 @@
 import 'package:anilist/anilist.dart';
 import 'package:test/test.dart';
+import 'package:dotenv/dotenv.dart';
 
 void main() {
-  final client = AnilistClient();
+  final env = DotEnv(includePlatformEnvironment: true)..load();
+  final client = AnilistClient(token: env['AL_TOKEN']);
 
   test("Ferry query test", () async {
-    final res = await client.mediaMinimal(search: "Seirei Gensouki");
+    const title = "Seirei Gensouki";
+    final res = await client.mediaMinimal(search: title);
+
+    if (res.isErr()) {
+      print(res.unwrapErr());
+      return;
+    }
+    final media = res.unwrap();
+    assert(media.title!.romaji == title);
+
+    print(res.unwrap());
+  });
+
+  test("Viewer test", () async {
+    final res = await client.currentUser();
 
     if (res.isErr()) {
       print(res.unwrapErr());
       return;
     }
 
-    print(res.unwrap());
+    final viewer = res.unwrap();
+    assert(viewer.name == "Yakiyo");
+    print(viewer);
   });
 }
