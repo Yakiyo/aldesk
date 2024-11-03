d70b7678258f5abacd927e8ea726b073f23cb160
Yakiyo <yakiyo1206@gmail.com>
2024-11-03 23:50:50 +0600

    feat: better splash page

--- lib/routes/splash_page.dart
+++ lib/routes/splash_page.dart
@@ -1,6 +1,8 @@
 import 'package:aldesk/util/colors.dart';
+import 'package:dart_jsonwebtoken/dart_jsonwebtoken.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
@@ -16,14 +18,55 @@ class _SplashPageState extends State<SplashPage> {
     initialize();
   }
 
-  void initialize() async {
-    Future.delayed(
-      const Duration(seconds: 2),
-      () {
-        if (!mounted) return;
-        context.go("/");
-      },
-    );
+  void initialize() {
+    Future.delayed(const Duration(seconds: 2), () {
+      final isloggedin = _isLoggedIn();
+      if (!mounted) return;
+      context.go(isloggedin ? "/" : "/login");
+    });
+  }
+
+  bool _isLoggedIn() {
+    final db = Hive.box<String>("db");
+    final tokenStr = db.get("token");
+
+    // if we dont have a token entry or an empty entry (shouldnt happen), must login
+    if (tokenStr == null || tokenStr.isEmpty) {
+      return false;
+    }
+
+    late final JWT jwt;
+
+    try {
+      jwt = JWT.decode(tokenStr).payload;
+    } catch (e) {
+      return false;
+    }
+
+    final now = DateTime.now();
+
+    /// issued at time
+    final iat = DateTime.fromMillisecondsSinceEpoch(jwt.payload["iat"] * 1000);
+
+    /// expire date
+    final exp = DateTime.fromMillisecondsSinceEpoch(jwt.payload["exp"] * 1000);
+
+    // iat < now < exp - this should be the proper serial
+
+    if (now.difference(iat).isNegative) {
+      return false;
+    }
+
+    final diff = exp.difference(now);
+    const minDiff = Duration(days: 1);
+
+    // if the exp date has passed or will pass within the duration of
+    // a day, we reject this
+    if (diff.isNegative || diff.compareTo(minDiff) < 0) {
+      return false;
+    }
+
+    return true;
   }
 
   @override
