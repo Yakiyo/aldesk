821d97e5f5b389fa45a4941084455e9c3da7e694
Yakiyo <yakiyo1206@gmail.com>
2024-11-03 23:50:13 +0600

    chore: remove redirect from router
    - we now check login infos at splash page and it is ensured to be valid for at least 24 hours

--- lib/router.dart
+++ lib/router.dart
@@ -3,12 +3,6 @@ import 'package:aldesk/routes/home_page.dart';
 import 'package:aldesk/routes/login_page.dart';
 import 'package:aldesk/routes/splash_page.dart';
 import 'package:go_router/go_router.dart';
-import 'package:hive_flutter/hive_flutter.dart';
-
-final _db = Hive.box<String>("db");
-final _isLoggedIn = _db.containsKey("token");
-// if the user is logged in, start with home page
-// otherwise redirect to the /login page
 
 final router = GoRouter(
   initialLocation: "/splash",
@@ -29,22 +23,5 @@ final router = GoRouter(
       builder: (context, state) => const SplashPage(),
     )
   ],
-  redirect: (context, state) {
-    if (state.matchedLocation == '/splash') {
-      return null;
-    }
-
-    // if user not logged in, move to login screen
-    if (!_isLoggedIn) {
-      return "/login";
-    }
-    // if user is logged in but still on the login page, move to home screen
-    if (state.matchedLocation == "/login" && _isLoggedIn) {
-      return "/";
-    }
-
-    // otherwise no redirection needed
-    return null;
-  },
   errorBuilder: (context, state) => const Page404(),
 );
