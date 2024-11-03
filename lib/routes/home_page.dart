fc4141499bb5c3c03a5be15ba2c9128313195c34
Yakiyo <yakiyo1206@gmail.com>
2024-11-03 23:50:25 +0600

    chore: try some stuff at home page

--- lib/routes/home_page.dart
+++ lib/routes/home_page.dart
@@ -10,8 +10,26 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       floatingActionButton: const NavigationFAB(),
       body: Container(
+        // alignment: Alignment.topLeft,
         color: colorBg,
+        padding: const EdgeInsets.only(top: 40, left: 50, right: 50),
+        child: ListView.builder(
+          itemCount: 3,
+          itemBuilder: (context, index) => const AiringBar(),
+        ),
       ),
     );
+  }
+}
+
+class AiringBar extends StatelessWidget {
+  const AiringBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // ignore: unused_local_variable
+    const img =
+        "https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx98977-CpZkd98fWj32.jpg";
+    return const Placeholder();
   }
 }
