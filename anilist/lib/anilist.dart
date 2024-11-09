31845af95a2a531c39a10ea0bd1fb4c2d3927190
Yakiyo <yakiyo1206@gmail.com>
2024-11-09 16:51:02 +0600

    chore: dart format

--- anilist/lib/anilist.dart
+++ anilist/lib/anilist.dart
@@ -4,4 +4,3 @@ library anilist;
 export 'src/anilist.dart';
 export 'src/request.dart' show ApiError;
 export 'src/graphql/__generated__/media.data.gql.dart';
-// export 'src/graphql/__generated__/schema.schema.gql.dart' show GMediaStatus;
