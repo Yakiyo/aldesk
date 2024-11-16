c4b86d42301fe14d46124e7504e08ad139ab521b
Yakiyo <yakiyo1206@gmail.com>
2024-11-17 02:56:10 +0600

    feat: export changes

--- anilist/lib/anilist.dart
+++ anilist/lib/anilist.dart
@@ -4,3 +4,4 @@ library anilist;
 export 'src/anilist.dart';
 export 'src/request.dart' show ApiError;
 export 'src/graphql/__generated__/media.data.gql.dart';
+export 'src/graphql/__generated__/activities.data.gql.dart';
