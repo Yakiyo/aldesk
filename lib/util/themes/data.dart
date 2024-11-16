fb58bd6d444ac486ecdf0b44bb9ed6b46fb638ba
Yakiyo <yakiyo1206@gmail.com>
2024-11-17 02:56:20 +0600

    chore: more additions to theme datas

--- lib/util/themes/data.dart
+++ lib/util/themes/data.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+const highlightColor = Color.fromRGBO(232, 93, 117, 1);
+
 const textTheme = TextTheme(
   bodyLarge: TextStyle(color: Color(0xFF9DABA9)),
   bodyMedium: TextStyle(color: Color(0xFF9DABA9)),
@@ -14,7 +16,9 @@ final ThemeData darkTheme = ThemeData(
   ),
   scaffoldBackgroundColor: const Color(0xFF0B1622),
   colorScheme: ColorScheme.fromSeed(
-      seedColor: const Color(0xFF152232), brightness: Brightness.dark),
+      seedColor: const Color(0xFF152232),
+      secondary: const Color.fromARGB(255, 170, 164, 164),
+      brightness: Brightness.dark),
 );
 
 final ThemeData lightTheme = ThemeData(
@@ -26,6 +30,7 @@ final ThemeData lightTheme = ThemeData(
   colorScheme: ColorScheme.fromSeed(
       seedColor: const Color(0xFFFAFAFA),
       primary: const Color(0xFFFAFAFA),
+      secondary: const Color.fromARGB(255, 3, 95, 244),
       brightness: Brightness.light),
   scaffoldBackgroundColor: const Color(0xFFEDF1F5),
 );
