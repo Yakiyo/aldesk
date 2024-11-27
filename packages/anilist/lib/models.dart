a650740f2b8df66bb4f7ac5d951f7c1e37d762e6
Yakiyo <yakiyo1206@gmail.com>
2024-11-27 17:23:35 +0600

    feat: script

---
+++ packages/anilist/lib/models.dart
@@ -0,0 +1,6 @@
+// GENERATED FILE BY `scripts.dart`. DO NOT MODIFY
+
+export 'src/models/activities.graphql.dart';
+export 'src/models/media.graphql.dart';
+export 'src/models/schema.graphql.dart';
+export 'src/models/user.graphql.dart';
