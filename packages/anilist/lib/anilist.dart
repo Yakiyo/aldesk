809445929473f483705d13ec0c3e5586984edde8
Yakiyo <yakiyo1206@gmail.com>
2024-11-27 16:55:11 +0600

    chore: empty file

--- packages/anilist/lib/anilist.dart
+++ packages/anilist/lib/anilist.dart
@@ -1,10 +1,4 @@
 /// Library for interacting with the Anilist Graphql API
 library;
 
-export 'src/request.dart';
-
-// generated files
-export 'src/generated/activities.graphql.dart';
-export 'src/generated/media.graphql.dart';
-export 'src/generated/schema.graphql.dart';
-export 'src/generated/user.graphql.dart';
+export 'src/anilist.dart';
