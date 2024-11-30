06b5faabe5362b678552528330b5f56d8bb6f16b
Yakiyo <yakiyo1206@gmail.com>
2024-12-01 02:35:24 +0600

    feat: markdown to html converter query

--- packages/anilist/lib/src/models/fragments.graphql.dart
+++ packages/anilist/lib/src/models/fragments.graphql.dart
@@ -4380,3 +4380,411 @@ class _CopyWithStubImplFragmentUserStatisticsFgenres<TRes>
   }) =>
       _res;
 }
+
+class VariablesQueryMdToHtml {
+  factory VariablesQueryMdToHtml({required String markdown}) =>
+      VariablesQueryMdToHtml._({
+        r'markdown': markdown,
+      });
+
+  VariablesQueryMdToHtml._(this._$data);
+
+  factory VariablesQueryMdToHtml.fromJson(Map<String, dynamic> data) {
+    final result$data = <String, dynamic>{};
+    final l$markdown = data['markdown'];
+    result$data['markdown'] = (l$markdown as String);
+    return VariablesQueryMdToHtml._(result$data);
+  }
+
+  Map<String, dynamic> _$data;
+
+  String get markdown => (_$data['markdown'] as String);
+
+  Map<String, dynamic> toJson() {
+    final result$data = <String, dynamic>{};
+    final l$markdown = markdown;
+    result$data['markdown'] = l$markdown;
+    return result$data;
+  }
+
+  CopyWithVariablesQueryMdToHtml<VariablesQueryMdToHtml> get copyWith =>
+      CopyWithVariablesQueryMdToHtml(
+        this,
+        (i) => i,
+      );
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) {
+      return true;
+    }
+    if (!(other is VariablesQueryMdToHtml) ||
+        runtimeType != other.runtimeType) {
+      return false;
+    }
+    final l$markdown = markdown;
+    final lOther$markdown = other.markdown;
+    if (l$markdown != lOther$markdown) {
+      return false;
+    }
+    return true;
+  }
+
+  @override
+  int get hashCode {
+    final l$markdown = markdown;
+    return Object.hashAll([l$markdown]);
+  }
+}
+
+abstract class CopyWithVariablesQueryMdToHtml<TRes> {
+  factory CopyWithVariablesQueryMdToHtml(
+    VariablesQueryMdToHtml instance,
+    TRes Function(VariablesQueryMdToHtml) then,
+  ) = _CopyWithImplVariablesQueryMdToHtml;
+
+  factory CopyWithVariablesQueryMdToHtml.stub(TRes res) =
+      _CopyWithStubImplVariablesQueryMdToHtml;
+
+  TRes call({String? markdown});
+}
+
+class _CopyWithImplVariablesQueryMdToHtml<TRes>
+    implements CopyWithVariablesQueryMdToHtml<TRes> {
+  _CopyWithImplVariablesQueryMdToHtml(
+    this._instance,
+    this._then,
+  );
+
+  final VariablesQueryMdToHtml _instance;
+
+  final TRes Function(VariablesQueryMdToHtml) _then;
+
+  static const _undefined = <dynamic, dynamic>{};
+
+  TRes call({Object? markdown = _undefined}) => _then(VariablesQueryMdToHtml._({
+        ..._instance._$data,
+        if (markdown != _undefined && markdown != null)
+          'markdown': (markdown as String),
+      }));
+}
+
+class _CopyWithStubImplVariablesQueryMdToHtml<TRes>
+    implements CopyWithVariablesQueryMdToHtml<TRes> {
+  _CopyWithStubImplVariablesQueryMdToHtml(this._res);
+
+  TRes _res;
+
+  call({String? markdown}) => _res;
+}
+
+class QueryMdToHtml {
+  QueryMdToHtml({
+    this.Markdown,
+    this.$__typename = 'Query',
+  });
+
+  factory QueryMdToHtml.fromJson(Map<String, dynamic> json) {
+    final l$Markdown = json['Markdown'];
+    final l$$__typename = json['__typename'];
+    return QueryMdToHtml(
+      Markdown: l$Markdown == null
+          ? null
+          : QueryMdToHtmlMarkdown.fromJson(
+              (l$Markdown as Map<String, dynamic>)),
+      $__typename: (l$$__typename as String),
+    );
+  }
+
+  final QueryMdToHtmlMarkdown? Markdown;
+
+  final String $__typename;
+
+  Map<String, dynamic> toJson() {
+    final _resultData = <String, dynamic>{};
+    final l$Markdown = Markdown;
+    _resultData['Markdown'] = l$Markdown?.toJson();
+    final l$$__typename = $__typename;
+    _resultData['__typename'] = l$$__typename;
+    return _resultData;
+  }
+
+  @override
+  int get hashCode {
+    final l$Markdown = Markdown;
+    final l$$__typename = $__typename;
+    return Object.hashAll([
+      l$Markdown,
+      l$$__typename,
+    ]);
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) {
+      return true;
+    }
+    if (!(other is QueryMdToHtml) || runtimeType != other.runtimeType) {
+      return false;
+    }
+    final l$Markdown = Markdown;
+    final lOther$Markdown = other.Markdown;
+    if (l$Markdown != lOther$Markdown) {
+      return false;
+    }
+    final l$$__typename = $__typename;
+    final lOther$$__typename = other.$__typename;
+    if (l$$__typename != lOther$$__typename) {
+      return false;
+    }
+    return true;
+  }
+}
+
+extension UtilityExtensionQueryMdToHtml on QueryMdToHtml {
+  CopyWithQueryMdToHtml<QueryMdToHtml> get copyWith => CopyWithQueryMdToHtml(
+        this,
+        (i) => i,
+      );
+}
+
+abstract class CopyWithQueryMdToHtml<TRes> {
+  factory CopyWithQueryMdToHtml(
+    QueryMdToHtml instance,
+    TRes Function(QueryMdToHtml) then,
+  ) = _CopyWithImplQueryMdToHtml;
+
+  factory CopyWithQueryMdToHtml.stub(TRes res) = _CopyWithStubImplQueryMdToHtml;
+
+  TRes call({
+    QueryMdToHtmlMarkdown? Markdown,
+    String? $__typename,
+  });
+  CopyWithQueryMdToHtmlMarkdown<TRes> get Markdown;
+}
+
+class _CopyWithImplQueryMdToHtml<TRes> implements CopyWithQueryMdToHtml<TRes> {
+  _CopyWithImplQueryMdToHtml(
+    this._instance,
+    this._then,
+  );
+
+  final QueryMdToHtml _instance;
+
+  final TRes Function(QueryMdToHtml) _then;
+
+  static const _undefined = <dynamic, dynamic>{};
+
+  TRes call({
+    Object? Markdown = _undefined,
+    Object? $__typename = _undefined,
+  }) =>
+      _then(QueryMdToHtml(
+        Markdown: Markdown == _undefined
+            ? _instance.Markdown
+            : (Markdown as QueryMdToHtmlMarkdown?),
+        $__typename: $__typename == _undefined || $__typename == null
+            ? _instance.$__typename
+            : ($__typename as String),
+      ));
+
+  CopyWithQueryMdToHtmlMarkdown<TRes> get Markdown {
+    final local$Markdown = _instance.Markdown;
+    return local$Markdown == null
+        ? CopyWithQueryMdToHtmlMarkdown.stub(_then(_instance))
+        : CopyWithQueryMdToHtmlMarkdown(
+            local$Markdown, (e) => call(Markdown: e));
+  }
+}
+
+class _CopyWithStubImplQueryMdToHtml<TRes>
+    implements CopyWithQueryMdToHtml<TRes> {
+  _CopyWithStubImplQueryMdToHtml(this._res);
+
+  TRes _res;
+
+  call({
+    QueryMdToHtmlMarkdown? Markdown,
+    String? $__typename,
+  }) =>
+      _res;
+
+  CopyWithQueryMdToHtmlMarkdown<TRes> get Markdown =>
+      CopyWithQueryMdToHtmlMarkdown.stub(_res);
+}
+
+const documentNodeQueryMdToHtml = DocumentNode(definitions: [
+  OperationDefinitionNode(
+    type: OperationType.query,
+    name: NameNode(value: 'MdToHtml'),
+    variableDefinitions: [
+      VariableDefinitionNode(
+        variable: VariableNode(name: NameNode(value: 'markdown')),
+        type: NamedTypeNode(
+          name: NameNode(value: 'String'),
+          isNonNull: true,
+        ),
+        defaultValue: DefaultValueNode(value: null),
+        directives: [],
+      )
+    ],
+    directives: [],
+    selectionSet: SelectionSetNode(selections: [
+      FieldNode(
+        name: NameNode(value: 'Markdown'),
+        alias: null,
+        arguments: [
+          ArgumentNode(
+            name: NameNode(value: 'markdown'),
+            value: VariableNode(name: NameNode(value: 'markdown')),
+          )
+        ],
+        directives: [],
+        selectionSet: SelectionSetNode(selections: [
+          FieldNode(
+            name: NameNode(value: 'html'),
+            alias: null,
+            arguments: [],
+            directives: [],
+            selectionSet: null,
+          ),
+          FieldNode(
+            name: NameNode(value: '__typename'),
+            alias: null,
+            arguments: [],
+            directives: [],
+            selectionSet: null,
+          ),
+        ]),
+      ),
+      FieldNode(
+        name: NameNode(value: '__typename'),
+        alias: null,
+        arguments: [],
+        directives: [],
+        selectionSet: null,
+      ),
+    ]),
+  ),
+]);
+
+class QueryMdToHtmlMarkdown {
+  QueryMdToHtmlMarkdown({
+    this.html,
+    this.$__typename = 'ParsedMarkdown',
+  });
+
+  factory QueryMdToHtmlMarkdown.fromJson(Map<String, dynamic> json) {
+    final l$html = json['html'];
+    final l$$__typename = json['__typename'];
+    return QueryMdToHtmlMarkdown(
+      html: (l$html as String?),
+      $__typename: (l$$__typename as String),
+    );
+  }
+
+  final String? html;
+
+  final String $__typename;
+
+  Map<String, dynamic> toJson() {
+    final _resultData = <String, dynamic>{};
+    final l$html = html;
+    _resultData['html'] = l$html;
+    final l$$__typename = $__typename;
+    _resultData['__typename'] = l$$__typename;
+    return _resultData;
+  }
+
+  @override
+  int get hashCode {
+    final l$html = html;
+    final l$$__typename = $__typename;
+    return Object.hashAll([
+      l$html,
+      l$$__typename,
+    ]);
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) {
+      return true;
+    }
+    if (!(other is QueryMdToHtmlMarkdown) || runtimeType != other.runtimeType) {
+      return false;
+    }
+    final l$html = html;
+    final lOther$html = other.html;
+    if (l$html != lOther$html) {
+      return false;
+    }
+    final l$$__typename = $__typename;
+    final lOther$$__typename = other.$__typename;
+    if (l$$__typename != lOther$$__typename) {
+      return false;
+    }
+    return true;
+  }
+}
+
+extension UtilityExtensionQueryMdToHtmlMarkdown on QueryMdToHtmlMarkdown {
+  CopyWithQueryMdToHtmlMarkdown<QueryMdToHtmlMarkdown> get copyWith =>
+      CopyWithQueryMdToHtmlMarkdown(
+        this,
+        (i) => i,
+      );
+}
+
+abstract class CopyWithQueryMdToHtmlMarkdown<TRes> {
+  factory CopyWithQueryMdToHtmlMarkdown(
+    QueryMdToHtmlMarkdown instance,
+    TRes Function(QueryMdToHtmlMarkdown) then,
+  ) = _CopyWithImplQueryMdToHtmlMarkdown;
+
+  factory CopyWithQueryMdToHtmlMarkdown.stub(TRes res) =
+      _CopyWithStubImplQueryMdToHtmlMarkdown;
+
+  TRes call({
+    String? html,
+    String? $__typename,
+  });
+}
+
+class _CopyWithImplQueryMdToHtmlMarkdown<TRes>
+    implements CopyWithQueryMdToHtmlMarkdown<TRes> {
+  _CopyWithImplQueryMdToHtmlMarkdown(
+    this._instance,
+    this._then,
+  );
+
+  final QueryMdToHtmlMarkdown _instance;
+
+  final TRes Function(QueryMdToHtmlMarkdown) _then;
+
+  static const _undefined = <dynamic, dynamic>{};
+
+  TRes call({
+    Object? html = _undefined,
+    Object? $__typename = _undefined,
+  }) =>
+      _then(QueryMdToHtmlMarkdown(
+        html: html == _undefined ? _instance.html : (html as String?),
+        $__typename: $__typename == _undefined || $__typename == null
+            ? _instance.$__typename
+            : ($__typename as String),
+      ));
+}
+
+class _CopyWithStubImplQueryMdToHtmlMarkdown<TRes>
+    implements CopyWithQueryMdToHtmlMarkdown<TRes> {
+  _CopyWithStubImplQueryMdToHtmlMarkdown(this._res);
+
+  TRes _res;
+
+  call({
+    String? html,
+    String? $__typename,
+  }) =>
+      _res;
+}
