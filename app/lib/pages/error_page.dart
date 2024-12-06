821e7504ea780cb370605206494445c9106bfeda
Yakiyo <yakiyo1206@gmail.com>
2024-12-06 18:42:46 +0600

    feat: error page

--- app/lib/pages/error_page.dart
+++ app/lib/pages/error_page.dart
@@ -1,10 +1,87 @@
+import 'package:aldesk/utils/assets.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
-class ErrorPage extends StatelessWidget {
+class ErrorPage extends StatefulWidget {
   const ErrorPage({super.key});
 
   @override
+  State<ErrorPage> createState() => _ErrorPageState();
+}
+
+class _ErrorPageState extends State<ErrorPage> with TickerProviderStateMixin {
+  late final AnimationController _controller = AnimationController(
+    lowerBound: 0.5,
+    duration: const Duration(seconds: 2),
+    vsync: this,
+  )..repeat(reverse: true);
+  late final Animation<double> _animation = CurvedAnimation(
+    parent: _controller,
+    curve: Curves.fastOutSlowIn,
+  );
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final style = Theme.of(context)
+        .textTheme
+        .headlineLarge
+        ?.copyWith(fontWeight: FontWeight.bold);
+    return Center(
+        child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        ScaleTransition(
+          scale: _animation,
+          child: Image.asset(
+            Assets.brain,
+            width: 200,
+            height: 200,
+          ),
+        ),
+        Text(
+          "404",
+          style: style?.copyWith(fontSize: 80, letterSpacing: 0.1),
+        ),
+        Text(
+          "Page Not Found",
+          style: style,
+        ),
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: TextButton(
+              onPressed: () => context.pop(),
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(10)),
+                padding:
+                    const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Icon(
+                      Icons.arrow_back,
+                      color: Colors.white,
+                    ),
+                    Text("Go Back",
+                        style: Theme.of(context)
+                            .textTheme
+                            .bodyMedium
+                            ?.copyWith(color: Colors.white)),
+                  ],
+                ),
+              )),
+        )
+      ],
+    ));
   }
 }
