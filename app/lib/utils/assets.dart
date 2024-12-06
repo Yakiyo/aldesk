b817d0e3f59a490562e7956beeff7c25b724ac22
Yakiyo <yakiyo1206@gmail.com>
2024-12-06 17:41:25 +0600

    feat: asset class

---
+++ app/lib/utils/assets.dart
@@ -0,0 +1,9 @@
+/// const string for asset paths
+abstract class Assets {
+  static const light404 = 'assets/images/404-light.jpg';
+  static const dark404 = 'assets/images/404-dark.jpg';
+  static const error404 = 'assets/images/404-dark.jpg';
+  static const aldeskLogo = 'assets/images/aldesk.png';
+  static const anilistLogo = 'assets/images/anilist.png';
+  static const anilistLongText = 'assets/images/anilist_long_text.png';
+}
