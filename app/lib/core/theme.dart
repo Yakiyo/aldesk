b6b09d2cc11b912d702ae87504a064908aef4aee
Yakiyo <yakiyo1206@gmail.com>
2024-12-16 19:13:25 +0600

    chore: add icon buttom theme

--- app/lib/core/theme.dart
+++ app/lib/core/theme.dart
@@ -3,7 +3,6 @@ import 'package:google_fonts/google_fonts.dart';
 
 import 'misc.dart';
 
-
 class ThemeManager {
   late final ValueNotifier<ThemeMode> _mode;
   ValueNotifier<ThemeMode> get modeNotifier => _mode;
@@ -28,42 +27,49 @@ class ThemeManager {
 
 const highlightColor = Color.fromRGBO(232, 93, 117, 1);
 
-final font = GoogleFonts.overpassTextTheme();
+final font = GoogleFonts.robotoTextTheme();
 
-/// Dark mode themes
-final darkTheme = ThemeData(
-  fontFamily: GoogleFonts.roboto().fontFamily,
-  textTheme: font.apply(
-    bodyColor: Colors.white,
-    displayColor: Colors.white,
-    decoration: TextDecoration.none,
-  ),
-  appBarTheme: const AppBarTheme(
-    backgroundColor: Color(0xFF152232),
-  ),
-  scaffoldBackgroundColor: const Color(0xFF0B1622),
-  colorScheme: ColorScheme.fromSeed(
+abstract class Themes {
+  static ThemeData dark = ThemeData(
+    fontFamily: GoogleFonts.roboto().fontFamily,
+    textTheme: font.apply(
+      bodyColor: Colors.white,
+      displayColor: Colors.white,
+      decoration: TextDecoration.none,
+    ),
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Color(0xFF152232),
+    ),
+    scaffoldBackgroundColor: const Color(0xFF0B1622),
+    colorScheme: ColorScheme.fromSeed(
       seedColor: const Color(0xFF152232),
       primary: const Color(0xFF152232),
       secondary: const Color.fromARGB(255, 170, 164, 164),
-      brightness: Brightness.dark),
-);
+      brightness: Brightness.dark,
+    ),
+    iconButtonTheme: const IconButtonThemeData(
+        style:
+            ButtonStyle(foregroundColor: WidgetStatePropertyAll(Colors.white))),
+  );
 
-/// Light mode themes
-final lightTheme = ThemeData(
-  fontFamily: GoogleFonts.roboto().fontFamily,
-  textTheme: font.apply(
-    bodyColor: Colors.grey,
-    displayColor: Colors.grey,
-    decoration: TextDecoration.none,
-  ),
-  appBarTheme: const AppBarTheme(
-    backgroundColor: Color.fromARGB(255, 43, 45, 66),
-  ),
-  scaffoldBackgroundColor: const Color(0xFFEDF1F5),
-  colorScheme: ColorScheme.fromSeed(
-      seedColor: const Color(0xFFFAFAFA),
-      primary: const Color(0xFFFAFAFA),
-      secondary: const Color.fromARGB(255, 3, 95, 244),
-      brightness: Brightness.light),
-);
+  static ThemeData light = ThemeData(
+      fontFamily: GoogleFonts.roboto().fontFamily,
+      textTheme: font.apply(
+        bodyColor: Colors.grey,
+        displayColor: Colors.grey,
+        decoration: TextDecoration.none,
+      ),
+      appBarTheme: const AppBarTheme(
+        backgroundColor: Color.fromARGB(255, 43, 45, 66),
+      ),
+      scaffoldBackgroundColor: const Color(0xFFEDF1F5),
+      colorScheme: ColorScheme.fromSeed(
+        seedColor: const Color(0xFFFAFAFA),
+        primary: const Color(0xFFFAFAFA),
+        secondary: const Color.fromARGB(255, 3, 95, 244),
+        brightness: Brightness.light,
+      ),
+      iconButtonTheme: const IconButtonThemeData(
+          style: ButtonStyle(
+              foregroundColor: WidgetStatePropertyAll(Colors.grey))));
+}
