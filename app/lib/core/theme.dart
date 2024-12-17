0077c29e4278b59d1e0f83b56737e957b7126383
Yakiyo <yakiyo1206@gmail.com>
2024-12-17 22:57:20 +0600

    chore: add primaryColor property

--- app/lib/core/theme.dart
+++ app/lib/core/theme.dart
@@ -41,10 +41,11 @@ abstract class Themes {
       backgroundColor: Color(0xFF152232),
     ),
     scaffoldBackgroundColor: const Color(0xFF0B1622),
+    primaryColor: const Color.fromARGB(255, 21, 31, 46),
     colorScheme: ColorScheme.fromSeed(
       seedColor: const Color(0xFF152232),
       // main color, the one used in containers and app bar and etc
-      primary: const Color(0xFF152232),
+      primary: const Color.fromARGB(255, 21, 31, 46),
       // same as scaffold background color
       secondary: const Color(0xFF0B1622),
       tertiary: const Color.fromARGB(255, 170, 164, 164),
@@ -66,6 +67,7 @@ abstract class Themes {
         backgroundColor: Color.fromARGB(255, 43, 45, 66),
       ),
       scaffoldBackgroundColor: const Color(0xFFEDF1F5),
+      primaryColor: const Color(0xFFFAFAFA),
       colorScheme: ColorScheme.fromSeed(
         seedColor: const Color(0xFFFAFAFA),
         primary: const Color(0xFFFAFAFA),
