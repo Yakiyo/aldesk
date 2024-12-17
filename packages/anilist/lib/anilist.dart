396de4dcc23d3bb43b0ad94eb5acdc4d8b6fb704
Yakiyo <yakiyo1206@gmail.com>
2024-12-17 22:57:05 +0600

    fix: export extensions

--- packages/anilist/lib/anilist.dart
+++ packages/anilist/lib/anilist.dart
@@ -6,5 +6,6 @@ export 'src/anilist.dart';
 export 'src/media.dart';
 export 'src/user.dart';
 export 'src/lists.dart';
+export 'src/extensions.dart';
 export 'package:gql/language.dart' show printNode;
 export 'package:option_result/result.dart' show Result, Ok, Err;
