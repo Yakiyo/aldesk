a692ee27f4a80880caff0fede598f465fffe3f09
Yakiyo <yakiyo1206@gmail.com>
2024-12-17 19:59:49 +0600

    feat: cache the response

--- packages/anilist/lib/src/user.dart
+++ packages/anilist/lib/src/user.dart
@@ -2,13 +2,29 @@ import 'consts.dart';
 import 'package:anilist/anilist.dart';
 import 'package:anilist/models.dart';
 
+FragmentUserMin? _viewerCache;
+
+/// associated token with the authenticated user.
+String? _token;
+
 /// Get the authenticated user.
 ///
 /// Requires to be authenticated, otherwise returns a [AuthError]
 ReturnType<FragmentUserMin> viewer() async {
   if (!isAuthed()) return Err(AuthError());
-  return request(query: printNode(documentNodeQueryViewer)).then(
-      (future) => future.map((value) => QueryViewer.fromJson(value).Viewer!));
+  final currentToken =
+      headers["Authorization"]!.replaceAll("Bearer ", "").trim();
+  // if cache is empty or token has changed, fetch the user
+  if (_viewerCache == null || _token != currentToken) {
+    final result = await request(query: printNode(documentNodeQueryViewer))
+        .then((future) =>
+            future.map((value) => QueryViewer.fromJson(value).Viewer!));
+    if (result.isErr()) return result;
+    _viewerCache = result.unwrap();
+    _token = currentToken;
+  }
+
+  return Ok(_viewerCache!);
 }
 
 /// Minimal info regarding a user. Either [id] or [search] must be provided.
