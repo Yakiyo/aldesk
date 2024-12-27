f22382d91c2e25ec8722426ef0c7542b78755b1c
Yakiyo <yakiyo1206@gmail.com>
2024-12-27 19:05:41 +0600

    chore: impromptu commit

--- app/lib/ui/pages/home.dart
+++ app/lib/ui/pages/home.dart
@@ -1,18 +1,33 @@
-import 'package:aldesk/ui/components/layout/my_scaffold.dart';
+import '../components/layout/my_scaffold.dart';
+import 'package:anilist/anilist.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../components/misc/async_widget.dart';
 
 class HomePage extends ConsumerWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final w = MediaQuery.sizeOf(context).width;
     return MyScaffold(
       body: Center(
         child: Column(
           mainAxisSize: MainAxisSize.min,
-          children: [Text("hello world $w")],
+          children: [
+            AsyncWidget(
+              future: trendingMedia(),
+              builder: (context, value) => Expanded(
+                child: ListView.builder(
+                  // scrollDirection: Axis.horizontal,
+                  itemCount: value.media?.length ?? 0,
+                  itemBuilder: (context, index) => Text(
+                      value.media![index]?.title?.userPreferred ??
+                          'Missing title'),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
