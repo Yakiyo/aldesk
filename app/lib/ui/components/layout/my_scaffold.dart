b11d8e83f5cdcf3f97ae333562f5f63cd98f6030
Yakiyo <yakiyo1206@gmail.com>
2024-12-27 21:10:15 +0600

    fix: remove that
    - fuck mobile view

--- app/lib/ui/components/layout/my_scaffold.dart
+++ app/lib/ui/components/layout/my_scaffold.dart
@@ -1,7 +1,6 @@
 import 'package:aldesk/ui/components/layout/fab.dart';
 import 'package:flutter/material.dart';
 
-import 'bottom_nav_bar.dart';
 import 'side_nav_bar.dart';
 
 class MyScaffold extends StatelessWidget {
@@ -10,28 +9,22 @@ class MyScaffold extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(builder: (context, constraints) {
-      final smallscreen = constraints.maxWidth < 800;
-
-      return Scaffold(
-        floatingActionButton: const Fab(),
-        bottomNavigationBar: smallscreen ? const BottomNavBar() : null,
-        body: Row(
-          children: [
-            if (!smallscreen)
-              const Expanded(
-                  flex: 1,
-                  child: Material(
-                    elevation: 10,
-                    child: SideNavBar(),
-                  )),
-            Expanded(
-              flex: 5,
-              child: body,
-            ),
-          ],
-        ),
-      );
-    });
+    return Scaffold(
+      floatingActionButton: const Fab(),
+      body: Row(
+        children: [
+          const Expanded(
+              flex: 1,
+              child: Material(
+                elevation: 10,
+                child: SideNavBar(),
+              )),
+          Expanded(
+            flex: 5,
+            child: body,
+          ),
+        ],
+      ),
+    );
   }
 }
