fd12fc845e3379e05a02b2eca24c3adc0700f38f
Yakiyo <yakiyo1206@gmail.com>
2024-12-27 18:42:26 +0600

    feat: more utils

--- app/lib/core/utils/get.dart
+++ app/lib/core/utils/get.dart
@@ -1,3 +1,6 @@
 import 'package:get_it/get_it.dart';
+import 'package:logger/logger.dart';
 
 T get<T extends Object>() => GetIt.I.get<T>();
+
+Logger get logger => GetIt.I.get<Logger>();
--- app/lib/ui/components/misc/async_widget.dart
+++ app/lib/ui/components/misc/async_widget.dart
@@ -1,10 +1,55 @@
-import 'package:flutter/widgets.dart';
+import 'package:async_builder/async_builder.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:toastification/toastification.dart';
 
-class AsyncWidget extends StatelessWidget {
-  const AsyncWidget({super.key});
+import '../../../core/utils/get.dart';
+import 'kaomoji.dart';
+
+/// A widget that builds itself based on the latest snapshot of a future.
+///
+/// This is basically a wrapper around [AsyncBuilder] that provides some defaults
+class AsyncWidget<T extends Object> extends StatelessWidget {
+  final Future<T> future;
+  final Widget Function(BuildContext context, T value) builder;
+  final WidgetBuilder loading;
+  final Widget Function(BuildContext, Object, StackTrace?) error;
+
+  const AsyncWidget(
+      {super.key,
+      required this.future,
+      required this.builder,
+      this.loading = _defaultLoading,
+      this.error = _defaultError});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return AsyncBuilder<T>(
+      future: future,
+      builder: (context, value) => builder(context, value!),
+      error: error,
+      waiting: loading,
+      silent: !kDebugMode,
+    );
   }
+}
+
+Widget _defaultLoading(BuildContext context) {
+  return const KaomojiLoader();
+}
+
+Widget _defaultError(
+    BuildContext context, Object error, StackTrace? stackTrace) {
+  toastification.show(
+    context: context,
+    type: ToastificationType.error,
+    style: ToastificationStyle.flat,
+    title: const Text("Error when fetching data from API"),
+  );
+  logger.e(
+    "Error when fetching data from API",
+    error: error,
+    stackTrace: stackTrace,
+  );
+  return const Icon(Icons.error);
 }
