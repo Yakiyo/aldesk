7aea2f399d7a841efb628f2e91fee475d4fe8850
Yakiyo <yakiyo1206@gmail.com>
2024-12-29 17:36:02 +0600

    feat: basic media list view for home page

---
+++ app/lib/ui/components/overview/media_list_view.dart
@@ -0,0 +1,128 @@
+import 'package:anilist/models.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class MediaListView extends StatelessWidget {
+  final String? title;
+  final String? seeMore;
+  final List<FragmentMediaMin> medias;
+  const MediaListView(
+      {super.key, required this.medias, this.title, this.seeMore});
+
+  @override
+  Widget build(BuildContext context) {
+    if (medias.isEmpty) {
+      return const SizedBox();
+    }
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            if (title != null)
+              Text(title!, style: Theme.of(context).textTheme.displaySmall),
+            if (seeMore != null)
+              IconButton(
+                onPressed: () {
+                  context.push(seeMore!);
+                },
+                icon: const Icon(Icons.open_in_new),
+              )
+          ],
+        ),
+        SizedBox(
+          height: 350,
+          child: ListView.builder(
+            itemCount: medias.length,
+            scrollDirection: Axis.horizontal,
+            itemBuilder: (context, index) {
+              final media = medias[index];
+              return _MediaEntry(media: media);
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _MediaEntry extends StatefulWidget {
+  final FragmentMediaMin media;
+  const _MediaEntry({required this.media});
+
+  @override
+  State<_MediaEntry> createState() => _MediaEntryState();
+}
+
+class _MediaEntryState extends State<_MediaEntry> {
+  double _scaleEnd = 1.0;
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        print("tapped");
+      },
+      onHover: (value) => setState(() => _scaleEnd = value ? 1.1 : 1.0),
+      child: TweenAnimationBuilder(
+        tween: Tween<double>(begin: 1.0, end: _scaleEnd),
+        duration: const Duration(milliseconds: 200),
+        builder: (context, value, child) => Transform.scale(
+          scale: value,
+          child: child,
+        ),
+        child: _MediaEntryBody(media: widget.media),
+      ),
+    );
+  }
+}
+
+class _MediaEntryBody extends StatelessWidget {
+  final FragmentMediaMin media;
+  const _MediaEntryBody({required this.media});
+
+  String? get _coverImage =>
+      media.coverImage?.large ?? media.coverImage?.medium;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 185,
+      padding: const EdgeInsets.all(10.0),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(5),
+            ),
+            clipBehavior: Clip.hardEdge,
+            child: _coverImage != null
+                ? Image.network(
+                    _coverImage ?? '',
+                    height: 265,
+                    width: 185,
+                    fit: BoxFit.cover,
+                  )
+                : Container(
+                    height: 265,
+                    width: 185,
+                    color: Colors.grey,
+                  ),
+          ),
+          Text(
+            media.title?.userPreferred ?? '',
+            softWrap: true,
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+            style: Theme.of(context).textTheme.bodyLarge,
+          ),
+        ],
+      ),
+    );
+  }
+}
--- app/lib/ui/pages/home.dart
+++ app/lib/ui/pages/home.dart
@@ -1,31 +1,55 @@
-import '../components/layout/my_scaffold.dart';
 import 'package:anilist/anilist.dart';
+import 'package:anilist/models.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../components/layout/my_scaffold.dart';
 import '../components/misc/async_widget.dart';
+import '../components/overview/media_list_view.dart';
 
-class HomePage extends ConsumerWidget {
+class HomePage extends ConsumerStatefulWidget {
   const HomePage({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends ConsumerState<HomePage> {
+  late final Future<List<FragmentMediaMin>> _trending;
+  late final Future<List<FragmentMediaMin>> _popular;
+
+  @override
+  void initState() {
+    _trending = trendingMedia().then(
+        (value) => value.media?.whereType<FragmentMediaMin>().toList() ?? []);
+    _popular = popularMedia().then(
+        (value) => value.media?.whereType<FragmentMediaMin>().toList() ?? []);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return MyScaffold(
-      body: Center(
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.only(left: 10),
         child: Column(
-          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.max,
           children: [
             AsyncWidget(
-              future: trendingMedia(),
-              builder: (context, value) => Expanded(
-                child: ListView.builder(
-                  // scrollDirection: Axis.horizontal,
-                  itemCount: value.media?.length ?? 0,
-                  itemBuilder: (context, index) => Text(
-                      value.media![index]?.title?.userPreferred ??
-                          'Missing title'),
-                ),
-              ),
+              future: _trending,
+              builder: (context, value) => MediaListView(
+                  title: 'Trending now',
+                  seeMore: "/search?sort=TRENDING_DESC",
+                  medias: value),
+            ),
+            AsyncWidget(
+              future: _popular,
+              builder: (context, value) => MediaListView(
+                  title: 'Popular all time',
+                  seeMore: "/search?sort=POPULAR_DESC",
+                  medias: value),
             ),
           ],
         ),
