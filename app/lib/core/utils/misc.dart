9a794032e462555fbea7ec82c34bd5670f16e254
Yakiyo <yakiyo1206@gmail.com>
2024-12-30 19:37:39 +0600

    chore: extension for color

--- app/lib/core/utils/misc.dart
+++ app/lib/core/utils/misc.dart
@@ -1,5 +1,13 @@
+import 'package:flutter/material.dart';
+
 extension Capitalize on String {
   String capitalize() {
     return "${this[0].toUpperCase()}${substring(1)}";
+  }
+}
+
+extension SetOpacity on Color {
+  Color setOpacity(double opacity) {
+    return withAlpha((opacity * 255).round());
   }
 }
