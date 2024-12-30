4c5d061e21da4346eca2c1e364a7e103cda41992
Yakiyo <yakiyo1206@gmail.com>
2024-12-30 19:40:27 +0600

    fix: fab issue

--- app/lib/ui/components/layout/fab.dart
+++ app/lib/ui/components/layout/fab.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../../themes/themes.dart';
+
 class Fab extends ConsumerWidget {
   const Fab({super.key});
 
@@ -8,10 +10,9 @@ class Fab extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final isLight = Theme.of(context).brightness == Brightness.light;
     return FloatingActionButton(
-      // onPressed: () {
-      //   ref.read(themeManagerProvider.notifier).toggle();
-      // },
-      onPressed: null,
+      onPressed: () {
+        updateTheme(isLight ? ThemeMode.dark : ThemeMode.light, ref);
+      },
       child: Icon(isLight ? Icons.light_mode : Icons.dark_mode),
     );
   }
