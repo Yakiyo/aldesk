f60ce2a7e1db4f7bafecc7ae1e7f8a2e0920a4b1
Yakiyo <yakiyo1206@gmail.com>
2025-01-02 20:53:54 +0600

    fix: add navigation

--- app/lib/ui/components/overview/forum_activity.dart
+++ app/lib/ui/components/overview/forum_activity.dart
@@ -67,102 +67,108 @@ class ThreadTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      color: Theme.of(context).colorScheme.secondary,
-      elevation: 10,
-      child: SizedBox(
-        width: 400,
-        height: 120,
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Row(
-            spacing: 10,
-            children: [
-              Expanded(
-                flex: 3,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      thread.title ?? "Missing title?",
-                      maxLines: 3,
-                      style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                          color: Theme.of(context).colorScheme.primary),
-                    ),
-                    RichText(
-                        maxLines: 2,
-                        text: TextSpan(
-                            style: TextStyle(
-                                color: Theme.of(context)
-                                    .textTheme
-                                    .bodyLarge
-                                    ?.color),
-                            children: [
-                              if (_replierAvatar != null)
-                                WidgetSpan(
-                                    child: Padding(
-                                  padding: const EdgeInsets.only(right: 5),
-                                  child: CircleAvatar(
-                                      radius: 10,
-                                      backgroundImage:
-                                          NetworkImage(_replierAvatar!)),
-                                )),
-                              TextSpan(
-                                  text: thread.replyUser?.name ?? "Unknown",
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      color: Theme.of(context)
-                                          .colorScheme
-                                          .primary)),
-                              TextSpan(text: " replied $_lastReplied")
-                            ])),
-                  ],
+    return InkWell(
+      hoverColor: Colors.transparent,
+      onTap: () {
+        context.go("/forum/${thread.id}");
+      },
+      child: Card(
+        color: Theme.of(context).colorScheme.secondary,
+        elevation: 10,
+        child: SizedBox(
+          width: 400,
+          height: 120,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              spacing: 10,
+              children: [
+                Expanded(
+                  flex: 3,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        thread.title ?? "Missing title?",
+                        maxLines: 3,
+                        style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                            color: Theme.of(context).colorScheme.primary),
+                      ),
+                      RichText(
+                          maxLines: 2,
+                          text: TextSpan(
+                              style: TextStyle(
+                                  color: Theme.of(context)
+                                      .textTheme
+                                      .bodyLarge
+                                      ?.color),
+                              children: [
+                                if (_replierAvatar != null)
+                                  WidgetSpan(
+                                      child: Padding(
+                                    padding: const EdgeInsets.only(right: 5),
+                                    child: CircleAvatar(
+                                        radius: 10,
+                                        backgroundImage:
+                                            NetworkImage(_replierAvatar!)),
+                                  )),
+                                TextSpan(
+                                    text: thread.replyUser?.name ?? "Unknown",
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        color: Theme.of(context)
+                                            .colorScheme
+                                            .primary)),
+                                TextSpan(text: " replied $_lastReplied")
+                              ])),
+                    ],
+                  ),
                 ),
-              ),
-              // ignore: prefer_const_constructors
-              Expanded(
-                flex: 1,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  // mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Wrap(
-                      spacing: 2,
-                      crossAxisAlignment: WrapCrossAlignment.center,
-                      children: [
-                        const Icon(Icons.remove_red_eye_sharp, size: 15),
-                        Text("$_views"),
-                        const Icon(Icons.chat_bubble_rounded, size: 15),
-                        Text("$_comments"),
-                      ],
-                    ),
-                    Wrap(
-                      children: [
-                        ..._categories.map((name) => Container(
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(5),
-                                color: _randColor(),
-                              ),
-                              padding: const EdgeInsets.all(2),
-                              child: Text(
-                                name,
-                                style: Theme.of(context)
-                                    .textTheme
-                                    .bodySmall
-                                    ?.copyWith(
-                                      color: Colors.white,
-                                      fontSize: 8,
-                                    ),
-                              ),
-                            ))
-                      ],
-                    ),
-                  ],
-                ),
-              )
-            ],
+                // ignore: prefer_const_constructors
+                Expanded(
+                  flex: 1,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    // mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Wrap(
+                        spacing: 2,
+                        crossAxisAlignment: WrapCrossAlignment.center,
+                        children: [
+                          const Icon(Icons.remove_red_eye_sharp, size: 15),
+                          Text("$_views"),
+                          const Icon(Icons.chat_bubble_rounded, size: 15),
+                          Text("$_comments"),
+                        ],
+                      ),
+                      Wrap(
+                        children: [
+                          ..._categories.map((name) => Container(
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(5),
+                                  color: _randColor(),
+                                ),
+                                padding: const EdgeInsets.all(2),
+                                child: Text(
+                                  name,
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .bodySmall
+                                      ?.copyWith(
+                                        color: Colors.white,
+                                        fontSize: 8,
+                                      ),
+                                ),
+                              ))
+                        ],
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
